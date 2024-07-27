9eb6f14003a8120a6e5d86757af07098bd12290b
zahidprvz <pervaizzahid55@gmail.com>
2024-07-27 18:32:31 +0500

    updated firebase rules

--- firebase.js
+++ firebase.js
@@ -1,19 +1,23 @@
 // Import the functions you need from the SDKs you need
 import { initializeApp } from "firebase/app";
 import { getAnalytics } from "firebase/analytics";
-import { getFirestore } from 'firebase/firestore';
-import { getStorage } from 'firebase/storage';
+import { getFirestore } from "firebase/firestore";
+import { getStorage } from "firebase/storage";
 
 // Your web app's Firebase configuration
-// For Firebase JS SDK v7.20.0 and later, measurementId is optional
+
 const firebaseConfig = {
-  apiKey: "AIzaSyClfxkJ4HSzM6eyvg7Ddr3M9xyPa_EcoPU",
-  authDomain: "zahidparvizweb.firebaseapp.com",
-  projectId: "zahidparvizweb",
-  storageBucket: "zahidparvizweb.appspot.com",
-  messagingSenderId: "1001317168361",
-  appId: "1:1001317168361:web:cf03eb764595fdafccac78",
-  measurementId: "G-Z9E933QQMD"
+  apiKey: "AIzaSyCT2HTK5O_OF00pQ8QvrZFUW7Yv4CRcYH0",
+
+  authDomain: "zahidparvizweb1.firebaseapp.com",
+
+  projectId: "zahidparvizweb1",
+
+  storageBucket: "zahidparvizweb1.appspot.com",
+
+  messagingSenderId: "535984254851",
+
+  appId: "1:535984254851:web:955ba869cf80a1364a50b6",
 };
 
 // Initialize Firebase
